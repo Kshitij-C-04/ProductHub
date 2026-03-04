86a8aa8d39a1f3b9640af2b4b298e4837004d72a
Kshitij-C-04 <kshitijchaware@gmail.com>
2026-03-04 11:22:53 +0530

    cleared conflicts

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,18 +1,9 @@
 import axios from "axios";
 
-<<<<<<< HEAD
-console.log("API URL:", import.meta.env.VITE_API_URL);
-=======
-
->>>>>>> 4c584083329d4d6d0013d991dc5b61556c416fe4
-
 const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true,
 });
 
-<<<<<<< HEAD
 export default api;
-=======
-export default api;
->>>>>>> 4c584083329d4d6d0013d991dc5b61556c416fe4
+
